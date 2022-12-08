69d443dd6d1a1e7b2a0dfae057c5b259fc6fa035
caotuankietc3a <caotuankietc3a@gmail.com>
2022-12-08 16:13:16 +0700

    [styles]: Add file Colors config

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary500: "#72063c",
+  primary600: "#640233",
+  primary700: "#4e0329",
+  primary800: "#3b021f",
+  accent500: "#ddb52f",
+  backdrop: "#000000",
+  color: "#ffffff",
+  placeholderColor: "#ffffff",
+  borderBottom: "#a5a5a5",
+  labelColor: "#a5a5a5",
+};
